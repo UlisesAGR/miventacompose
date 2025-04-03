5a6327f594b8f6b4020dfa2d72b7a6dcd14a38c5
ulises <kingluslises00@gmail.com>
2025-04-03 04:44:33 -0600

    [Improvement][Ulises] Move login activity file

--- app/src/main/java/com/miventa/compose/mobile/presentation/start/ui/login/view/LoginActivity.kt
+++ app/src/main/java/com/miventa/compose/mobile/presentation/start/ui/login/view/LoginActivity.kt
@@ -3,7 +3,7 @@
  * Created by Ulises Gonzalez
  * Copyright (c) 2025. All rights reserved
  */
-package com.miventa.compose.mobile.miventa.login.ui.view.activity
+package com.miventa.compose.mobile.presentation.start.ui.login.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,9 +11,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import com.miventa.compose.mobile.R
-import com.miventa.compose.mobile.miventa.login.ui.navigation.LoginNavigationWrapper
-import com.miventa.compose.mobile.miventa.login.ui.viewmodel.LoginViewModel
-import com.miventa.compose.mobile.miventa.order.ui.view.activity.OrderActivity
+import com.miventa.compose.mobile.presentation.start.ui.login.navigation.LoginNavigationWrapper
+import com.miventa.compose.mobile.presentation.start.viewmodel.login.LoginViewModel
+import com.miventa.compose.mobile.presentation.order.OrderActivity
 import com.miventa.compose.mobile.theme.Screen
 import com.miventa.compose.mobile.util.nextActivityEnd
 import dagger.hilt.android.AndroidEntryPoint
